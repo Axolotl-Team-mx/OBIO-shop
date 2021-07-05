2810f70aafddd5ef713fb368c08e0ee671ebf922
LuisGildardoAlvarado <ve15268@innovaccion.mx>
2021-07-05 16:08:43 -0700

    brand Views and urls

--- base/urls/brand_urls.py
+++ base/urls/brand_urls.py
@@ -1,3 +1,12 @@
 from base.views import brand_views as views
+from django.urls import path
 
-urlpatterns = []
+urlpatterns = [
+    path('getbrands/', views.getBrands, name='getBrands'),
+    path('createbrand/', views.createBrand, name='createBrand'),
+    path('getbrand/<str:pk>/', views.getBrand, name='getBrand'),
+    path('deletebrand/<str:pk>/', views.deleteBrand, name='deleteBrand'),
+    path('updatebrand/<str:pk>/', views.updateBrand, name='updateBrand'),
+]
+    
+
